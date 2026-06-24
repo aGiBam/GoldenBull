9290410fa41730427d338bae3b579f2a040c54b4
MohamedAgiba <mohamed.agiba@cic.ae>
2026-06-24 18:04:41 +0300

    feat: initialize application structure with routing, feature modules, and Transloco internationalization support

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Home } from './@features/home/home';
+import { Products } from './@features/products/products';
+import { Cart } from './@features/cart/cart';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./@features/home/home').then((c) => c.Home),
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./@features/products/products').then((c) => c.Products),
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./@features/cart/cart').then((c) => c.Cart),
+  },
+  { path: '**', redirectTo: 'home' },
+];

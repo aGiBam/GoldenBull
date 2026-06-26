7c5b4e0d332198c3cfa7177b497813361eebfcdf
MohamedAgiba <mohamed.agiba@cic.ae>
2026-06-26 08:33:16 +0300

    feat: initialize application structure with PrimeNG integration and Transloco internationalization support

--- src/app/@shared/navbar/navbar.ts
+++ src/app/@shared/navbar/navbar.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { TranslocoModule } from '@jsverse/transloco';
+import { TranslocoService } from '@jsverse/transloco';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [TranslocoModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  private transloco = inject(TranslocoService);
+  switchLang() {
+    this.transloco.setActiveLang(this.transloco.getActiveLang() === 'en' ? 'ar' : 'en');
+  }
+}
